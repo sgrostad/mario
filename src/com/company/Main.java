ea493ee677b3e70458ffcbafcae77bc92dd3a699
sindre <sindre.grostad@icloud.com>
2018-02-25 22:49:07 +0000

    First experimenting with sprites

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
 
-public class Main {
+
+public class Main extends JFrame {
+
+    public Main() {
+
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame ex = new Main();
+                ex.setVisible(true);
+            }
+        });
     }
 }
